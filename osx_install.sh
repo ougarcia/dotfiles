23a21f8b6ad61af505a5596b52371077dc639b4d
Oscar Garcia <ogarcia@23andme.com>
2017-06-30 10:46:35 -0700

    Update package installation

--- osx_install.sh
+++ osx_install.sh
@@ -1,56 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#use the following line to install xcode
-#xcode-select --install
+###############################################################################
+# XCode Command Line Tools                                                    #
+###############################################################################
+# From: https://github.com/nicksp/dotfiles and https://github.com/alrra/dotfiles
+
+if ! xcode-select --print-path &> /dev/null; then
+
+  # Prompt user to install the XCode Command Line Tools
+  xcode-select --install &> /dev/null
+
+  # Wait until the XCode Command Line Tools are installed
+  until xcode-select --print-path &> /dev/null; do
+    sleep 5
+  done
+
+  # Point the `xcode-select` developer directory to
+  # the appropriate directory from within `Xcode.app`
+  # https://github.com/alrra/dotfiles/issues/13
+  sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+
+  # Automatically accept xcode license.
+  sudo xcodebuild -license accept &> /dev/null
+  echo 'Agreed with the XCode Command Line Tools licence'
+
+fi
 
 #install homebrew & command line tools
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install git
-brew install heroku-toolbelt
-brew install postgres
-brew install rbenv
-brew install rbenv-gem-rehash
-brew install ruby-build
-brew install vim
-brew install tmux
-brew install zsh
-brew install the_silver_searcher
-brew install tree
-brew install reattach-to-user-namespace
-brew install httpie
-brew install fpp
-
-#install brew-cask & gui tools
-brew tap caskroom/cask
-
-brew cask install google-chrome
-brew cask install atom
-brew cask install postgres
-brew cask install flux
-brew cask install vlc
-brew cask install iterm2
-brew cask install evernote
-brew cask install slack
-brew cask install karabiner-elements
-brew cask install postman
-brew cask install hipchat
-
-
-#get ruby up and running
-#might eventually want to add the —verbose flag, usually takes a while.
-rbenv install 2.2.4
-rbenv global 2.2.4
-
-
-#install node and jshint. Eventually I’ll want to use nvm
-brew install node
-npm install -g jshint
+brew bundle
 
 #install oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #install dotfiles
 bash ~/.dotfiles/dotfiles_install.sh
@@ -64,19 +46,13 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
 ~/.tmux/plugins/tpm/bin/install_plugins
 
-#install important gems
-gem update --system
-gem install bundler rails rubocop
-
-# Install fonts
-brew tap caskroom/fonts
-brew cask install font-source-code-pro
-
 # Install the `pure` zsh theme
-rm ~/.oh-my-zsh/themes/pure.zsh-theme
-git clone https://github.com/sindresorhus/pure.git
-cp pure/pure.zsh ~/.oh-my-zsh/custom/pure.zsh-theme
-cp pure/async.zsh ~/.oh-my-zsh/custom/async.zsh
+git clone https://github.com/sindresorhus/pure.git /tmp/pure
+cp /tmp/pure/pure.zsh ~/.oh-my-zsh/custom/pure.zsh-theme
+cp /tmp/pure/async.zsh ~/.oh-my-zsh/custom/async.zsh
 
-rm -rf pure/
-
+# Ruby stuff
+rbenv install 2.4.1 --verbose
+rbenv global 2.4.1
+gem update --system
+gem install bundler rubocop
