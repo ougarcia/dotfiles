919ccd79eedde660abc6b906f97c73c4809e8551
Oscar Garcia <ogarcia@23andme.com>
2017-06-29 22:56:59 -0700

    Remove no-longer-used configs

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,22 +1,15 @@
-#!/bin/bash
-ln -sf ~/.dotfiles/aliases ~/.aliases
+#!/usr/bin/env bash
+
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/gemrc ~/.gemrc
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/gitignore ~/.gitignore
-ln -sf ~/.dotfiles/inputrc ~/.inputrc
 ln -sf ~/.dotfiles/irbrc ~/.irbrc
-ln -sf ~/.dotfiles/jshintrc ~/.jshintrc
 ln -sf ~/.dotfiles/profile ~/.profile
 ln -sf ~/.dotfiles/pryrc ~/.pryrc
-ln -sf ~/.dotfiles/rdebugrc ~/.rdebugrc
-ln -sf ~/.dotfiles/rspec ~/.rspec
 ln -sf ~/.dotfiles/rubocop.yml ~/.rubocop.yml
-ln -sf ~/.dotfiles/sqliterc ~/.sqliterc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/tmux_theme.conf ~/.tmux_theme.conf
 ln -sf ~/.dotfiles/eslintrc ~/.eslintrc
 ln -sf ~/.dotfiles/agignore ~/.agignore
 ln -sf ~/.dotfiiles/karabiner.json ~/.config/karabiner/karabiner.json
