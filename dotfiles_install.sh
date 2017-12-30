a18607d67b803a596489a9773dd043402b842d85
Oscar Garcia <ogarcia@23andme.com>
2017-12-29 21:33:58 -0800

    Remove unused config files

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-ln -sf ~/.dotfiles/bashrc ~/.bashrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/gitignore ~/.gitignore
-ln -sf ~/.dotfiles/irbrc ~/.irbrc
-ln -sf ~/.dotfiles/profile ~/.profile
 ln -sf ~/.dotfiles/pryrc ~/.pryrc
-ln -sf ~/.dotfiles/rubocop.yml ~/.rubocop.yml
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/eslintrc ~/.eslintrc
-ln -sf ~/.dotfiles/agignore ~/.agignore
 ln -sf ~/.dotfiiles/karabiner.json ~/.config/karabiner/karabiner.json
