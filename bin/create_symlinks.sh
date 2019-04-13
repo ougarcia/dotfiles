836add11132471ffc5736d1390351c39d3aacfe7
Oscar Garcia <hello@ogarcia.io>
2019-04-13 16:56:12 -0700

    Don't assume directory name

--- bin/create_symlinks.sh
+++ bin/create_symlinks.sh
@@ -11,17 +11,18 @@ DOTFILES=(
   "zshrc"
 )
 
-# Config files that follow xdg directory spec
-XDG_CONFIGS="$HOME/.dotfiles/config/*"
+DOTFILES_DIRECTORY=$(realpath "$PWD/..")
 
 for dotfile in "${DOTFILES[@]}"
 do
-  ln -sf "$HOME/.dotfiles/$dotfile" "$HOME/.$dotfile"
+  ln -sf "$DOTFILES_DIRECTORY/$dotfile" "$HOME/.$dotfile"
 done
 
+# Config files that follow xdg directory spec
+XDG_CONFIGS="$DOTFILES_DIRECTORY/config/*"
 
 for directory in $XDG_CONFIGS
 do
-  filename="$(basename $directory)"
+  filename="$(basename "$directory")"
   ln -sf "$directory" "$HOME/.config/$filename"
 done
