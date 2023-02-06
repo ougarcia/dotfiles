4004ee5d3b2c1020fc6c0eca30d8e7eea2e04955
Oscar Garcia <hello@ogarcia.io>
2023-02-06 15:32:57 -0800

    Update wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,3 +1,12 @@
 return {
   color_scheme = "Gruvbox dark, hard (base16)",
+  enable_tab_bar = false,
+  font_size = 13.0,
+  line_height = 1.1,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  }
 }
