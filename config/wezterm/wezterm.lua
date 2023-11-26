59477b9539171333291dd5e87427d970ddac2856
Oscar Garcia <hello@ogarcia.io>
2023-11-26 12:59:03 -0800

    Modernize wezterm config and use Berkeley Mono

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,12 +1,21 @@
-return {
-  color_scheme = "Monokai Pro (Gogh)",
-  enable_tab_bar = false,
-  font_size = 13.0,
-  line_height = 1.1,
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  }
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Monokai Pro (Gogh)'
+config.enable_tab_bar = false
+config.font_size = 13.0
+config.line_height = 1.2
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
+config.font = wezterm.font 'Berkeley Mono'
+
+return config
