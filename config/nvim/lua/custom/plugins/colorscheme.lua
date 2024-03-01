b24bc5252bb26a738a7e0c50d48e6a08491a0dba
Oscar Garcia <hello@ogarcia.io>
2024-03-01 09:11:56 -0800

    Use newest version of kickstarter

--- config/nvim/lua/custom/plugins/colorscheme.lua
+++ config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   'loctvl842/monokai-pro.nvim',
-  priority = 1000,
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     vim.cmd.colorscheme 'monokai-pro'
   end,
--- config/nvim/lua/custom/plugins/lualine.lua
+++ config/nvim/lua/custom/plugins/lualine.lua
@@ -10,7 +10,7 @@ return {
       section_separators = '',
     },
     sections = {
-      lualine_c = { { 'filename', path = 1 } }
+      lualine_c = { { 'filename', path = 1 } },
     },
   },
 }
