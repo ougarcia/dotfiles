176c907be229fb0e4156e288a538f33e18563836
Oscar Garcia <hello@ogarcia.io>
2024-03-01 15:17:38 -0800

    Replace lualine with barbeque

---
+++ config/nvim/lua/custom/plugins/statusline.lua
@@ -0,0 +1,13 @@
+return {
+  'utilyre/barbecue.nvim',
+  name = 'barbecue',
+  version = '*',
+  dependencies = {
+    'SmiteshP/nvim-navic',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('barbecue').setup()
+    vim.opt.laststatus = 0
+  end,
+}
