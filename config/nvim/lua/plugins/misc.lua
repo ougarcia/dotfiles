31884f5376129b9622f231e8fb492c0b32562b91
Oscar Garcia <hello@ogarcia.io>
2024-06-13 19:39:11 -0700

    Change symbols used for gitsigns

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -5,18 +5,7 @@ return {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
   -- git  signs in gutter, also utilities for managing changes
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  { 'lewis6991/gitsigns.nvim', opts = {} },
   {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
